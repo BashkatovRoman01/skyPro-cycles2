93c06c191a4d3f72a4c3049d99bae9bc79f3ec92
User <bashkatovr@mail.ru>
2022-11-24 22:53:30 +0300

    commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        System.out.println("Задание 1");
+        int salary=15000;
+        int total=0;
+        for (int i=0; total <=2_459_000 ; i++) {
+            total=total+salary;
+            System.out.println("Месяц " + i+ ", сумма накоплений равна " + total + " рублей");
+        }
     }
 }
