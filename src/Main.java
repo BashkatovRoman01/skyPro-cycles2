df7a1d8e5333dc97e1de039b16decb2b86a9c43c
User <bashkatovr@mail.ru>
2022-11-25 18:28:33 +0300

    commit 1

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,12 @@ public class Main {
     public static void main(String[] args) {
         // Задание 1
         System.out.println("Задание 1");
-        int salary=15000;
-        int total=0;
-        for (int i=0; total <=2_459_000 ; i++) {
-            total=total+salary;
-            System.out.println("Месяц " + i+ ", сумма накоплений равна " + total + " рублей");
+        double salary = 15000;
+        double percent = 0.01;
+        double total = 0;
+        for (int i = 0; total <= 2_459_000; i++) {
+            total = (total + salary) * (1 + percent);
+            System.out.println("Месяц " + i + " , сумма накоплений равна " + total + " рублей. ");
         }
     }
 }
